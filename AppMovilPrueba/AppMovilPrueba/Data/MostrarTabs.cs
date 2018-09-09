5143fa17dfbdc4c6f1fc002581d0e04f84947263
DESKTOP-ODJ18FD\Lucas <lucasjaraespinoza@gmail.com>
2018-09-09 11:22:22 -0300

    prueba replace

--- AppMovilPrueba/AppMovilPrueba/Data/MostrarTabs.cs
+++ AppMovilPrueba/AppMovilPrueba/Data/MostrarTabs.cs
@@ -11,8 +11,8 @@ namespace AppMovilPrueba.Data
     {
         public MostrarTabs()
         {
-            Children.Add(new BuscadorImagenes { Title = "Mostrar Imagen", Icon = "foto.png" });
-            Children.Add(new MostrarMapa { Title = "Mostrar Mapa", Icon = "mapa.png" });
+            Children.Add(new BuscadorImagenes { Title = "Mostrar Imagen ", Icon = "foto.png" });
+            Children.Add(new MostrarMapa { Title = "Mostrar Mapa ", Icon = "mapa.png" });
         }
     }
 }
